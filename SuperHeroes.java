6fbf3118365be07075831d4166c6a2efa87ccf84
Macarena Lara Molina <molina.macarenalara@gmail.com>
2022-02-27 23:43:41 -0300

    controller

--- SuperHeroes.java
+++ SuperHeroes.java
@@ -1,17 +1,15 @@
 package com.codecademy.ccapplication;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.lang.Iterable;
 import java.util.Date;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
-@RequestMapping("/superHeroes")
+@GetMapping("/superHeroes")
 public class SuperHeroController {
 
   private final SuperHeroRepository superHeroRepository;
@@ -28,29 +26,16 @@ public class SuperHeroController {
     return superHeroes;
 	}
 
-@PostMapping(path="/addNew")
-public String createNewSuperHero(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String superPower) {
-  SuperHero newSuperHero = new SuperHero(firstName, lastName, superPower);
-  superHeroRepository.save(newSuperHero);
-  return "New Super Hero successfully added!";
-}
-
-@PostMapping(path="/help")
-public String postHelp(@RequestParam String postalCode, @RequestParam String streetAddress) {
-  SuperReport newSuperReport = new SuperReport(postalCode, streetAddress, "");
-  superReportRepository.save(newSuperReport);
-  return "Thanks! Super Heroes have been dispatched to your location!";
-}
-
-@GetMapping(path="/heroReport")
+@GetMapping(value = "/heroReport")
 public Iterable<SuperReport> getHeroReport() {
   Iterable<SuperReport> superReport = superReportRepository.findAll();
   return superReport;
 }
 
-@PostMapping(path="/{postalCode}")
-public Iterable<SuperReport> getHeroReportByPostal(@PathVariable String postalCode) {
-  Iterable<SuperReport> superReport = superReportRepository.findByPostalCode(postalCode);
-  return superReport;
+@PutMapping(value = "/addNew")
+public String createNewSuperHero(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String superPower) {
+  SuperHero newSuperHero = new SuperHero(firstName, lastName, superPower);
+  superHeroRepository.save(newSuperHero);
+  return "New Super Hero successfully added!";
 }
 }
